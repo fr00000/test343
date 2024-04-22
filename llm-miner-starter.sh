57c37f7165f5cf4d3bc33582412335415624e8cf
fr00000 <faisal233@gmail.com>
2024-04-22 04:29:10 -0400

    Update llm-miner-starter.sh

--- llm-miner-starter.sh
+++ llm-miner-starter.sh
@@ -259,7 +259,7 @@ validateVram() {
     elif [[ "$heurist_model_id" == *"yi-34b-gptq"* ]] && [ "$available_mb" -gt 38000 ]; then
         local gpu_memory_util=$(echo "scale=2; (38000-1000)/$available_mb" | bc)
     elif [[ "$heurist_model_id" == *"pro-mistral-7b"* ]] && [ "$available_mb" -gt 18000 ]; then
-        local gpu_memory_util=$(echo "scale=2; (18000-1000)/$available_mb" | bc)
+        local gpu_memory_util=$(echo "scale=2; (22000-1000)/$available_mb" | bc)
     else
         local gpu_memory_util=$(echo "scale=2; (12000-1000)/$available_mb" | bc) # Default value or handle other cases as needed
     fi
